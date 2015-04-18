37e3a161a2f9c7a2660afb8c579dc64b9879004b
seescode <seesyong@gmail.com>
2015-04-18 11:17:54 -0400

    Got the initial links page up and working

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,16 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
+.config(function($stateProvider, $urlRouterProvider) {
 
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'templates/links.html'
+  })
+
+  $urlRouterProvider.otherwise('/')
+
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
