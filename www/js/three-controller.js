c7f4429f18dcf9c5cff6c27e69578740f6a0c6e7
seescode <seesyong@gmail.com>
2015-04-19 23:23:50 -0400

    Example with a long url

--- www/js/three-controller.js
+++ www/js/three-controller.js
@@ -2,15 +2,15 @@
 
 (function () {
     angular.module('starter')
-      .controller('OneController', function ($scope, $firebaseObject) {
+      .controller('ThreeController', function ($scope, $firebaseObject) {
           var vm = this;
 
-          var ref = new Firebase("https://radiant-torch-6366.firebaseio.com/");
+          var ref = new Firebase("https://radiant-torch-6366.firebaseio.com/three/cow/bell");
 
           var syncObject = $firebaseObject(ref);
 
           //It looks like we can name "data" to whatever we want.  It doesn't
           //effect any of the key/value names on firebase.
-          syncObject.$bindTo($scope, "data");
+          syncObject.$bindTo($scope, "zodd");
       });
 })();
