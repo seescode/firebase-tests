e6329c3bd65d529167a397a0bc697ba8b00958df
seescode <seesyong@gmail.com>
2015-05-02 17:22:59 -0400

    Adding logout functionality and also provided user login info

--- www/js/ten-controller.js
+++ www/js/ten-controller.js
@@ -1,28 +1,53 @@
 'use strict';
 
 (function () {
-    angular.module('starter')
-      .controller('TenController', function ($scope, $firebaseObject, $firebaseAuth) {
-          var vm = this;
+  angular.module('starter')
+    .controller('TenController', function ($scope, $firebaseObject, $firebaseAuth) {
+    var vm = this;
 
-          var ref = new Firebase("https://radiant-torch-6366.firebaseio.com/ten");
-          
-          var auth = $firebaseAuth(ref);
-          
-          // login with Facebook
-          auth.$authWithOAuthPopup("google").then(function(authData) {
-            console.log("Logged in as:", authData.uid);
-            $scope.loggedIn = true;
-          }).catch(function(error) {
-            console.log("Authentication failed:", error);
-          });
-          
+    var ref = new Firebase("https://radiant-torch-6366.firebaseio.com/ten");
 
+    var auth = $firebaseAuth(ref);
 
-          var syncObject = $firebaseObject(ref);
+    var updateStatus = function () {
+      var authData = auth.$getAuth();
 
-          //It looks like we can name "data" to whatever we want.  It doesn't
-          //effect any of the key/value names on firebase.
-          syncObject.$bindTo($scope, "data");
+      if (authData) {
+        $scope.uid = authData.uid;
+        $scope.provider = authData.provider;
+        $scope.auth = authData.auth;
+        $scope.expires = authData.expires;
+        console.log(authData)
+      } else {
+        $scope.uid = "Not logged in";
+        $scope.provider = '';
+        $scope.auth = '';
+        $scope.expires = '';
+      }
+    };
+    
+    vm.login = function () {           
+      // login with Facebook
+      auth.$authWithOAuthPopup("google").then(function (authData) {
+        console.log("Logged in as:", authData.uid);
+        $scope.loggedIn = true;
+        updateStatus();
+      }).catch(function (error) {
+        console.log("Authentication failed:", error);
       });
+    }
+
+    vm.logout = function () {
+      auth.$unauth();
+      updateStatus();      
+    }
+
+    var syncObject = $firebaseObject(ref);
+
+    //It looks like we can name "data" to whatever we want.  It doesn't
+    //effect any of the key/value names on firebase.
+    syncObject.$bindTo($scope, "data");
+    
+    updateStatus();
+  });
 })();
