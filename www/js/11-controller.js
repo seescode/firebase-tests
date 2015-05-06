be811ad9cc102e9bc19381d4dbb638e3fdf1c1ab
seescode <seesyong@gmail.com>
2015-05-05 22:02:49 -0400

    Fixed bug where when you login > logout > login with someone else, it updates data for both users

--- www/js/11-controller.js
+++ www/js/11-controller.js
@@ -7,7 +7,7 @@
 
     var ref = new Firebase("https://radiant-torch-6366.firebaseio.com/11");
     var auth = $firebaseAuth(ref);
-
+    var syncObject = null;
 
     var updateStatus = function () {
       var authData = auth.$getAuth();
@@ -15,7 +15,12 @@
       if (authData) {                
         $scope.uid = authData.uid;
         ref = new Firebase("https://radiant-torch-6366.firebaseio.com/11/" + authData.uid);
-        var syncObject = $firebaseObject(ref);
+        
+        if (syncObject != null) {
+          syncObject.$destroy();
+        }
+        
+        syncObject = $firebaseObject(ref);
         syncObject.$bindTo($scope, "data");
       } else {
         $scope.uid = "";
@@ -35,6 +40,7 @@
 
     vm.logout = function () {
       auth.$unauth();
+      
       updateStatus();      
     }
 
